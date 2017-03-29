e5b0270973460eb87c72ee0fbfd54d21805af2c9
xiaomoxu <xxu@rs.com>
2017-03-29 18:14:11 +0800

    Create spring cloud server Create crawler project based on Crawler4j

---
+++ crawler-service/src/main/java/com/rocket/crawler/CrawlerApplication.java
@@ -0,0 +1,19 @@
+package com.rocket.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@EnableEurekaClient
+public class CrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+
+}
